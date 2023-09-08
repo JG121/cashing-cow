6ad66e9f6ddebe5222c4c592cfce295153584dbf
JG121 <loopyplayz12@gmail.com>
2023-09-08 16:44:48 -0400

    Second Commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,5 +3,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <ClerkProvider><Component {...pageProps} /> </ClerkProvider>
+  return <ClerkProvider>
+    <Component {...pageProps} /> 
+    </ClerkProvider>
 }
